32d1cab71847ce3d04fa42a28c451d948945f057
Hein Zaw Htet <maungheinzawhtet@gmail.com>
2012-10-12 13:23:31 +0630

    Update config.php

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
 	$config = array(
 
-		"host" => "localhost",	// Host name
-		"user" => "root", 		// Database user
-		"pass" => "",			// Database password
-		"name" => "cms",			// Database Name
+		"host" => "tunnel.pagodabox.com:3306",	// Host name
+		"user" => "alyson", 		// Database user
+		"pass" => "fP0qOv7c",			// Database password
+		"name" => "mmwebdevcmstuts_phpfogapp_com",			// Database Name
 
 		"site_name" => "My Own Custom CMS",
-		"site_url" => "http://localhost/cms",
-		"ads_img" => "http://localhost/cms/assets/images/ads.jpg",
+		"site_url" => "http://myanmarwebdevcmstut.pagodabox.com/",
+		"ads_img" => "http://myanmarwebdevcmstut.pagodabox.com/assets/images/ads.jpg",
 		"ads_url" => "http://www.myanmarwebdev.com"
 	);
 	
-	$conn = new MySQLi($config['host'], $config['user'], $config['pass'], $config['name']) or die(mysqli_error());
+	$conn = new mysqli($config['host'], $config['user'], $config['pass'], $config['name']) or die(mysqli_error());
