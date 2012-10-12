2f862678042aed787d078ddb746c2fc89c014d7e
Hein Zaw Htet <maungheinzawhtet@gmail.com>
2012-10-12 13:45:05 +0630

    Change DB info

--- config.php
+++ config.php
@@ -1,10 +1,11 @@
 <?php
 	$config = array(
 
-		"host" => "tunnel.pagodabox.com:3306",	// Host name
+		"host" => "tunnel.pagodabox.com",	// Host name
 		"user" => "wilda", 		// Database user
 		"pass" => "35zqsKSv",			// Database password
 		"name" => "mmwebdevcmstuts_phpfogapp_com",			// Database Name
+		"port" => "3306",
 
 		"site_name" => "My Custom CMS",
 		"site_url" => "http://myanmarwebdevcmstut.pagodabox.com/",
@@ -12,4 +13,4 @@
 		"ads_url" => "http://www.myanmarwebdev.com"
 	);
 	
-	$conn = new mysqli($config['host'], $config['user'], $config['pass'], $config['name']) or die(mysqli_error());
+	$conn = new mysqli($config['host'], $config['user'], $config['pass'], $config['name'], $config['port']) or die(mysqli_error());
